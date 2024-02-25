ee9412fcca897a9e9fb78487c4f60dbc9e1ef94e
antuabinash <150573436+antuabinash@users.noreply.github.com>
2024-02-25 21:47:41 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function playAudio(audioId, imageName) {
-    const audio = document.getElementById(audioId);
-    audio.play();
-
-    // You can do something with the image name, like displaying it somewhere
-    console.log('Clicked on:', imageName);
-}
